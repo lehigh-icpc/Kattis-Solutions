c9d7d904b68ee6c510cc0336bfbcd001db24c90c
Evan Hruskar <hruskar.evan@gmail.com>
2019-04-27 01:46:23 -0400

    Create carrots-evan.py

---
+++ carrots/carrots-evan.py
@@ -0,0 +1,5 @@
+'''
+This problem teaches you that you can ignore all irrelivant input (even if not accepting all input breaks your console)
+'''
+n, p = [int(x) for x in input().split(' ')]
+print(p)
